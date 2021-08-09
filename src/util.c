ef15b11822ac39ac64ffef5b95aa85e9512d55e7
Entraff <thomasvivas@icloud.com>
2021-08-09 00:47:49 -0400

    Added linear algebra utilities

---
+++ src/util.c
@@ -0,0 +1,12 @@
+#include "util.h"
+
+bool vec2cmp(struct Vector2 a, struct Vector2 b) {
+    return a.x == b.x &&
+           a.y == b.y;
+}
+
+struct Vector2 vec2add(struct Vector2 a, struct Vector2 b) {
+    const int x = a.x + b.x;
+    const int y = a.y + b.y;
+    return (struct Vector2){ .x = x, .y = y };
+}
---
+++ src/util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "stdbool.h"
+
+struct Vector2 {
+    int x, y;
+};
+
+// Determines if two vectors have the same values
+bool vec2cmp(struct Vector2 a, struct Vector2 b);
+struct Vector2 vec2add(struct Vector2 a, struct Vector2 b);
+
+#endif // UTIL_H
