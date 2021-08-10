34fd068239dc975242deb5a10d93c0b9a1105239
Entraff <thomasvivas@icloud.com>
2021-08-10 18:50:19 -0400

    Changed Vector2 functions to accept pointers as arguments and added Vector2 allocation function

--- src/util.c
+++ src/util.c
@@ -1,12 +1,21 @@
 #include "util.h"
 
-bool vec2cmp(struct Vector2 a, struct Vector2 b) {
-    return a.x == b.x &&
-           a.y == b.y;
+struct Vector2 *vec2_init(const int x, const int y) {
+    struct Vector2 *vec2 = malloc(sizeof(struct Vector2));
+
+    vec2->x = x;
+    vec2->y = y;
+
+    return vec2;
 }
 
-struct Vector2 vec2add(struct Vector2 a, struct Vector2 b) {
-    const int x = a.x + b.x;
-    const int y = a.y + b.y;
+bool vec2cmp(struct Vector2 *a, struct Vector2 *b) {
+    return a->x == b->x &
+           a->y == b->y;
+}
+
+struct Vector2 vec2add(struct Vector2 *a, struct Vector2 *b) {
+    const int x = a->x + b->x;
+    const int y = a->y + b->y;
     return (struct Vector2){ .x = x, .y = y };
 }
--- src/util.h
+++ src/util.h
@@ -8,8 +8,8 @@ struct Vector2 {
     int x, y;
 };
 
-// Determines if two vectors have the same values
-bool vec2cmp(struct Vector2 a, struct Vector2 b);
-struct Vector2 vec2add(struct Vector2 a, struct Vector2 b);
+struct Vector2 *vec2_init(const int x, const int y);
+bool vec2cmp(struct Vector2 *a, struct Vector2 *b);
+struct Vector2 vec2add(struct Vector2 *a, struct Vector2 *b);
 
 #endif // UTIL_H
