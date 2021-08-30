d839bcd72e83aa053df6094949345d11caf3540c
Entraff <thomasvivas@icloud.com>
2021-08-30 17:46:52 -0400

    Removed unimplemented and no longer needed function from a header file

--- src/util.h
+++ src/util.h
@@ -8,7 +8,6 @@ struct Vector2 {
     int x, y;
 };
 
-struct Vector2 vec2_init(const int x, const int y);
 bool vec2cmp(struct Vector2 a, struct Vector2 b);
 struct Vector2 vec2add(struct Vector2 a, struct Vector2 b);
 
